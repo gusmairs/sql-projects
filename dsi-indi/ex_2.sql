16ada943705da97b5f7ac85c192809ae2936107f
gusmairs <gusmairs@mac.com>
2019-06-14 23:22:15 -0700

    converted ex 1 to cte method

--- dsi-indi/ex_2.sql
+++ dsi-indi/ex_2.sql
@@ -1,4 +1,4 @@
--- Fast version creates daily count table first then sums
+-- Fast version creates daily count table first then sums with self-join
 --
 with day_count as (
    select dt, count(userid) as registered
@@ -9,10 +9,3 @@ select a.dt, sum(b.registered)
 from day_count a join day_count b on b.dt <= a.dt
 group by a.dt order by a.dt
 ;
-
--- Slow because self-join traverses whole list over and over
---
--- select ua.dt, count(distinct ub.userid) as cnt
--- from users ua join users ub on ub.dt <= ua.dt
--- group by ua.dt order by ua.dt
--- ;
