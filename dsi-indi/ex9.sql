56b741120447997d527a87d5cc8988606ab8ef1f
Gus <gusmairs@mac.com>
2019-06-19 17:21:27 -0700

    just a tweak on ex7-9

--- dsi-indi/ex9.sql
+++ dsi-indi/ex9.sql
@@ -1,10 +1,10 @@
 -- Find all the users who bought a meal before liking or sharing a meal.
 
--- select distinct e1.userid, e1.meal_id, e1.dt, e1.event, e2.dt, e2.event
--- from events e1 join events e2 on
---    e1.userid = e2.userid and
---    e1.meal_id = e2.meal_id and
---    e1.dt < e2.dt
--- where e1.event = 'bought' and e2.event in ('like', 'share')
--- order by 1, 3
--- ;
+select distinct e1.userid, e1.meal_id, e1.dt, e1.event, e2.dt, e2.event
+from events e1 join events e2 on
+   e1.userid = e2.userid and
+   e1.meal_id = e2.meal_id and
+   e1.dt < e2.dt
+where e1.event = 'bought' and e2.event in ('like', 'share')
+order by 1, 3
+;
