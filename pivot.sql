d8b6598ea56e7c8c442ce69ce7094d020c40d6ac
gusmairs <gusmairs@mac.com>
2019-06-21 23:38:53 -0700

    maybe cross join helps

--- pivot.sql
+++ pivot.sql
@@ -1,17 +1,38 @@
 -- create temp table pivt as (
 --    select
---       meal_id as meal,
+--       'm-' || meal_id as meal,
 --       type,
---       rank() over (partition by type order by meal_id) as alph
+--       rank() over (partition by type order by meal_id) as rw
 --    from meals
 --    where type in ('italian', 'french', 'chinese') and meal_id <= 20
---    order by 3, 2
+--    order by 2, 3
+-- )
+-- ;
+-- select
+--    row_number() over (partition by null) as idx,
+--    *
+-- from pivt
+-- ;
+
+-- create temp table ital as (
+--    select rw, meal from pivt where type = 'italian'
+-- )
+-- ;
+-- create temp table french as (
+--    select rw, meal from pivt where type = 'french'
+-- )
+-- ;
+-- create temp table chinese as (
+--    select rw, meal from pivt where type = 'chinese'
 -- )
 -- ;
 
-select
-   array_agg (case when type = 'french' then meal end) as chi
-   -- array_agg (case when type = 'french' then meal end) as fre,
-   -- array_agg (case when type = 'italian' then meal end) as ita
-from pivt
+select distinct * from (
+   select
+      ital.meal ita,
+      french.meal fre
+      -- chinese.meal chi
+   from ital cross join french cross join chinese
+) x
+order by 1, 2
 ;
