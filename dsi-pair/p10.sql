72293878a8e55aa4af707ee5627c359193bfda7e
Gus <gusmairs@mac.com>
2019-06-23 17:14:32 -0700

    working on p10

--- dsi-pair/p10.sql
+++ dsi-pair/p10.sql
@@ -1,7 +1,7 @@
 -- Break the users into 10 cohorts based on their number of friends
 -- and get the average number of messages for each group
 
-create table active_summary as
+create temp table if not exists activity as
    select user_id, frnds, count(message) msgs
    from (
       select f.userid1 user_id, count(f.userid2) frnds
@@ -14,4 +14,24 @@ create table active_summary as
    ) t
    join messages m on t.user_id = m.recipient
    group by 1, 2 order by 1
+;
+
+-- select
+--    user_id,
+--    frnds,
+--    ntile(10) over (partition by frnds order by frnds) bucket
+-- from activity
+-- order by 3, 2
+-- ;
+
+with tmp as (
+   select
+      frnds fr,
+      count(user_id) ct,
+      sum(count(user_id)) over (order by frnds) sm,
+      ntile(10) over (order by 1) nt
+   from activity
+   group by frnds
+)
+select *, sm::int / 100 bucket from tmp
 ;
