6b08ee22a2139fa40bf9ffb0e17cff154b84e601
Gus <gusmairs@mac.com>
2019-06-23 22:41:18 -0700

    completed p10 extra credit

--- dsi-pair/p10.sql
+++ dsi-pair/p10.sql
@@ -1,10 +1,12 @@
 -- Break the users into 10 cohorts based on their number of friends
 -- and get the average number of messages for each group
 
-create temp table if not exists activity as
-   select user_id, frnds, count(message) msgs
+drop table activity
+;
+create temp table activity as
+   select id, fds, count(message) msgs
    from (
-      select f.userid1 user_id, count(f.userid2) frnds
+      select f.userid1 id, count(f.userid2) fds
       from (
          select * from friends
          except
@@ -12,26 +14,34 @@ create temp table if not exists activity as
       ) f
       group by 1 order by 1
    ) t
-   join messages m on t.user_id = m.recipient
+   join messages m on t.id = m.recipient
    group by 1, 2 order by 1
 ;
 
--- select
---    user_id,
---    frnds,
---    ntile(10) over (partition by frnds order by frnds) bucket
--- from activity
--- order by 3, 2
--- ;
-
-with tmp as (
-   select
-      frnds fr,
-      count(user_id) ct,
-      sum(count(user_id)) over (order by frnds) sm,
-      ntile(10) over (order by 1) nt
-   from activity
-   group by frnds
-)
-select *, sm::int / 100 bucket from tmp
+select grp, round(avg(msgs)) m_sum, round(sum(msgs) / sum(fds)) m_rte from (
+   select *, cume::int / 100 grp from (
+      select
+         fds,
+         msgs,
+         count(id) hist,
+         sum(count(id)) over (order by fds) cume
+      from activity
+      group by 1, 2
+   ) x
+) y
+group by 1 order by 1
 ;
+
+grp | m_sum | m_rte
+----+-------+-------
+  0 |   276 |    79
+  1 |   353 |    64
+  2 |   390 |    56
+  3 |   421 |    53
+  4 |   481 |    53
+  5 |   506 |    51
+  6 |   553 |    50
+  7 |   612 |    51
+  8 |   629 |    47
+  9 |   697 |    42
+(10 rows)
