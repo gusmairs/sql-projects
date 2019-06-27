f0ab5abb9a0a97d1f61e3497e4d4ff0317915227
gusmairs <gusmairs@mac.com>
2019-06-26 22:49:16 -0700

    sql works inside psql but not from py

---
+++ dsi-python/pg_pipe_full.py
@@ -0,0 +1,11 @@
+import psycopg2
+from datetime import datetime
+
+conn = psycopg2.connect(dbname='socialmedia')
+c = conn.cursor()
+today = datetime.strptime('2014-08-14', '%Y-%m-%d').strftime("%Y%m%d")
+with open('dsi-python/social.sql', 'r') as q:
+    query = q.read().replace('\n', '')
+c.execute(query)
+conn.commit()
+conn.close()
--- dsi-python/social.sql
+++ dsi-python/social.sql
@@ -1,47 +1,24 @@
--- create table logins_7d as (
-   -- select
-   --    lg.userid,
-   --    date(reg.tmstmp) as reg_date,
-   --    '2014-08-14' - date(max(lg.tmstmp)) as latest,
-   --    count(distinct '2014-08-14' - date(lg.tmstmp)) last_7
-   -- from registrations reg join logins lg
-   --    on reg.userid = lg.userid and '2014-08-14' - date(lg.tmstmp) < 7
-   -- group by 1, 2
-   -- order by 1
--- )
--- ;
+with
+   last_7 as (
+      select distinct
+         userid,
+         '2014-08-14' - date(tmstmp) log_days,
+         type log_type
+      from logins
+      where '2014-08-14' - date(tmstmp) < 7
+      order by 1, 2
+   )
+   ,
 
--- select
---    lg.userid,
---    date(reg.tmstmp) as reg_date,
---    '2014-08-14' - date(max(lg.tmstmp)) as latest
--- from registrations reg join logins lg on reg.userid = lg.userid
--- group by 1, 2 order by 1
--- ;
-
-drop table if exists last_7
-;
-
-create temp table last_7 as (
-   select distinct
-      userid,
-      '2014-08-14' - date(tmstmp) log_days,
-      type log_type
-   from logins
-   where '2014-08-14' - date(tmstmp) < 7
-   order by 1, 2
-)
-;
-
-with tally as (
-   select
-      userid,
-      count(distinct log_days) svn,
-      sum(case when log_type = 'mobile' then 1 else 0 end) phn,
-      sum(case when log_type = 'web' then 1 else 0 end) web
-   from last_7
-   group by 1 order by 1
-)
+   tally as (
+      select
+         userid,
+         count(distinct log_days) svn,
+         sum(case when log_type = 'mobile' then 1 else 0 end) phn,
+         sum(case when log_type = 'web' then 1 else 0 end) web
+      from last_7
+      group by 1 order by 1
+   )
 
 select
    r.userid id,
