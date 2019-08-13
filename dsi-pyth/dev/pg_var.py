3213863624b15e7c0d2681ed0103cc7ed3b79ecb
gusmairs <gusmairs@mac.com>
2019-08-12 22:33:59 -0700

    small changes to refine

--- dsi-pyth/dev/pg_var.py
+++ dsi-pyth/dev/pg_var.py
@@ -1,4 +1,8 @@
 # Refined from the dsi starter code
+# Parameters passed with the '%s' marker, values held in a tuple following
+# the query text object. Parameters may also be named as '%(<name>)s' and
+# in this case the values are held in a dictionary format following the
+# query text object, as '{<name>: <value>}'
 
 import psycopg2
 
